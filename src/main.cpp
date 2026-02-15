c28f5be8d1223a074fe01c0e3bb900b9c7cc6364
Alexander Chen <alexanderchen817@gmail.com>
2026-02-15 15:25:01 -0800

    add button functionality for up/down/left/right

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,36 @@
 #include <Arduino.h>
 
-#define LED 2
+const int upButton = 13;
+const int downButton = 12;
+const int rightButton = 14;
+const int leftButton = 27;
+
 void setup() {
-  // put your setup code here, to run once:
+  // intiailize 4 buttons 
   Serial.begin(115200);
-  pinMode(LED,OUTPUT);
+  pinMode(upButton, INPUT_PULLUP);
+  pinMode(downButton, INPUT_PULLUP);
+  pinMode(rightButton, INPUT_PULLUP);
+  pinMode(leftButton, INPUT_PULLUP);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  Serial.println("LED IS ON");
-  delay(1000);
-  digitalWrite(LED, LOW);
-  Serial.println("LED IS OFF");
-  delay(1000);
+  // read the button state
+  // not pressed down = 0
+  // pressed = 1
+  int upState = !digitalRead(upButton);
+  int downState = !digitalRead(downButton);
+  int rightState = !digitalRead(rightButton);
+  int leftState = !digitalRead(leftButton);
+  Serial.print("Up Button: ");
+  Serial.println(upState);
+  Serial.print("Down Button: ");
+  Serial.println(downState);
+  Serial.print("right Button: ");
+  Serial.println(rightState);
+  Serial.print("left Button: ");
+  Serial.println(leftState);
+
+  // avoid wasting resources 
+  delay(100);
 }
